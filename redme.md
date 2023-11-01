42c24ea2f3e8db6ad9ff78c15b9840fc7055c3ef
mihaipaulorban <mihaipaulorban@gmail.com>
2023-11-01 15:01:30 +0100

    feat: un filo di bootstrap e favicon

--- redme.md
+++ redme.md
@@ -1,3 +1,5 @@
 # Esercizio allert js
 
-questo esercizio è incentrato sulla creazione di allert con js, che chiedono all'utente di inserire prorpie informaioni per generare una password non sicura.
+Questo esercizio è incentrato sulla creazione di allert con js, che chiedono all'utente di inserire prorpie informaioni per generare una password non sicura.
+
+Ho utilizzato "functions collectData()" per far si che gli allert apparissero solo quando l'utente schiaccia il pulsante e per generare il numero casuale ho utilizzato "Math.floor(Math.random() \* 101)" per far si che moltiplicasse per 101 a caso il numero 1, dando come risultato un numero casuale tra 0 e 100.
--- script.js
+++ script.js
@@ -1,15 +1,21 @@
+// Funzione per far apparire le allert box solo quando l'utente schiaccia il pulsante
 function collectData() {
+
+    // Allert box
     let name = prompt("Inserisci il tuo nome: ");
     let surname = prompt("Inserisci il tuo cognome: ");
     let age = parseInt(prompt("Inserisci la tua età: "));
    
-    // Generate a random number between 0 and 100
+    // Genera un numero a caso tra 0 e 100
     let randomNumber = Math.floor(Math.random() * 101);
 
+    // Unisce le variabili senza spazi
     let gpassword = name + surname + age + randomNumber;
 
+    // Mostra il risultato all'utente
     document.getElementById("passwordDisplay").innerText = "Password Generata: " + gpassword;
    
+    // Mostra le informazioni fornite nella console
     console.log("Nome:", name);
     console.log("Cognome:", surname);
     console.log("Età:", age);
