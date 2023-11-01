16894998755ba6fe913ac7986409253bd4a27428
mihaipaulorban <mihaipaulorban@gmail.com>
2023-11-01 14:25:02 +0100

    feat:generatore funzionante + bonus numero casuale

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-function collectInformation() {
-    var name = prompt("Inserisci il tuo nome:");
-    var surname = prompt("Inserisci il tuo cognome:");
-    var age = prompt("Inserisci la tua età:");
+function collectData() {
+    let name = prompt("Inserisci il tuo nome: ");
+    let surname = prompt("Inserisci il tuo cognome: ");
+    let age = parseInt(prompt("Inserisci la tua età: "));
+   
+    // Generate a random number between 0 and 100
+    let randomNumber = Math.floor(Math.random() * 101);
 
-    var result = document.getElementById("result");
-    result.innerHTML = "Nome: " + name + ", Cognome: " + surname + ", Età: " + age;
-}
+    let gpassword = name + surname + age + randomNumber;
+
+    document.getElementById("passwordDisplay").innerText = "Password Generata: " + gpassword;
+   
+    console.log("Nome:", name);
+    console.log("Cognome:", surname);
+    console.log("Età:", age);
+    console.log("Numero casuale:", randomNumber);
+    console.log("Password Generata:", gpassword);
+   }
